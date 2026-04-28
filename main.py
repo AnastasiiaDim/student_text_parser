f7629a543bdb83deadaad9483e94eaed11630bef
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-04-28 14:37:21 +0200

    add: process the files in Student Text Parser

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import re
 import json
 import logging
+import os
 
 # Set up logging
-
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("parser.log"), logging.StreamHandler()],
 )
+
+#
+# Process the files
+
+student_folder = "C:\\Users\\Анастасия\\Desktop\\python_codes_in_txt\\student_texts"
+student_files = []
+student_contents = []
+
+if not os.path.exists(student_folder):
+    raise FileNotFoundError(f"Check your path: {student_folder}")
+
+for file_name in os.listdir(student_folder):
+    full_path = os.path.join(student_folder, file_name)
+
+    if os.path.isfile(full_path):
+        student_files.append(file_name)
+
+        with open(full_path, "r") as f:
+            student_contents.append(f.read())
+
+student_texts = dict(zip(student_files, student_contents))
+print(student_texts)
